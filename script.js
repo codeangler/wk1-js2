df9eef4a47b5d6c4a74ce5a0ebe594cc5870b9dd
Casey Burnett <burnett.casey@gmail.com>
2016-05-19 11:49:06 -0600

    completed JS2

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 var submission = prompt ("Pleaes enter any word");
 
-var characterCount = submission.length();
+var characterCount = submission.length;
+var thirdCharacter = submission.charAt(2);
+var nLowercase = submission.toLowerCase();
+var nUppercase = submission.toUpperCase();
+var nSubword = submission.substring(1,4);
+
 alert("You entered: " + submission 
-	+ "\nThere are " + characterCount + " characters in the word." +
+	+ "\nThere are " + characterCount	 + " characters in the word." +
 	"\nThe third character is '" + thirdCharacter + "'" +
 	"\nLowercase: " + nLowercase +
 	"\nUppercase: " + nUppercase +
 	"\nExample: " + "I have wanted a " + submission + " since I was a little girl." +
 	"\nSubword: " + nSubword);
+	
