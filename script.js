32c4e296956ad0e0a0ecb8966a6f623312d1e41b
Casey Burnett <burnett.casey@gmail.com>
2016-05-19 11:35:59 -0600

    JS alert modifed

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var submission = prompt ("Pleaes enter any word");
 
 var characterCount = submission.length();
+alert("You entered: " + submission 
+	+ "\nThere are " + characterCount + " characters in the word." +
+	"\nThe third character is '" + thirdCharacter + "'" +
+	"\nLowercase: " + nLowercase +
+	"\nUppercase: " + nUppercase +
+	"\nExample: " + "I have wanted a " + submission + " since I was a little girl." +
+	"\nSubword: " + nSubword);
